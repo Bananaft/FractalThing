036dd8069619622a7c8984d5c7dad8f4fde4b866
Bananaft <Bananaft@gmail.com>
2016-09-29 01:22:06 +0500

    so much optimization

--- bin/Data/Shaders/GLSL/raymarch.glsl
+++ bin/Data/Shaders/GLSL/raymarch.glsl
@@ -50,43 +50,47 @@ void PS()
   vec3 normal;
   vec3 intersection;
   //vec3 direction = camMat * normalize( vec3(uv.xy,2.0) );
+  float PREdepth = texture2D(sSpecMap, vScreenPos).r;
+
   float distance = 0.;
-  float totalDistance = cNearClipPS;
+  float totalDistance = PREdepth;// * cFarClipPS;
   float lfog = 0.;
   float pxsz = fov * cGBufferInvSize.y;
 
-  float distTrsh = 0.02;
+  float distTrsh = 0.002;
+  int stps = 0;
 
   for(int i =0 ;  i < cRAY_STEPS; ++i) ////// Rendering main scene
    {
        intersection = origin + direction * totalDistance;
 
        distance = sdfmap2(intersection);
-
+      totalDistance += distance;
        #ifdef PREMARCH
-          distTrsh = pxsz * totalDistance * 1.3;
+          distTrsh = pxsz * totalDistance * 1.4142;
        #endif
        if(distance <= distTrsh || totalDistance >= cFarClipPS)
        {
            break;
        }
-       totalDistance += distance;
+
+       stps = i;
    }
    vec4 clpp = vec4(intersection,1.0) * cViewProjPS;
    float fdepth = clpp.z /(cFarClipPS);
 
    #ifndef PREMARCH
+
       vec3 diffColor = vec3(0.5 + sin(intersection.y * 0.6) * 0.3,0.6 + sin(intersection.z * 0.2) * 0.3,1.0);
 
       vec3 ambient = diffColor.rgb;
 
       float depth = DecodeDepth(texture2D(sDepthBuffer, vScreenPos).rgb);
-      float depth16 = DecodeDepth(texture2D(sSpecMap, vScreenPos).rgb);
 
       if (fdepth>depth) discard;
 
-      float mimus = max( -1000000. * (fdepth - depth16), 0.0 );
-      float plus = max( 100. * (fdepth - depth16), 0.0 );
+      float mimus = max( -1000000. * (fdepth - PREdepth), 0.0 );
+      float plus = max( 10. * (fdepth - PREdepth), 0.0 );
       //Normal softening powered by magic.
       normal = calcNormal(intersection, max(pow(clpp.z,1.3) * pxsz,0.001));
 
@@ -95,11 +99,11 @@ void PS()
 
   //gl_FragColor = vec4(ambient , 1.0);
   #ifndef PREMARCH
-    gl_FragData[0] = vec4(mimus , plus,0.,0.);
-    gl_FragData[1] = vec4(diffColor.rgb * fog, 1.7 );
+    gl_FragData[0] = vec4(float(stps)/256,0.,0.,0.);//vec4(vec3(0.3) * (1.- fog),1.0);//vec4(float(stps)/cRAY_STEPS,0.,0.,0.);//vec4(mimus , plus,0.,0.); //vec4(vec3(0.3) * (1.-fog),1.0);
+    gl_FragData[1] = vec4(0.);//vec4(diffColor.rgb * fog, 1.7 );
     gl_FragData[2] = vec4(normal, 1.0);// * 0.5 + 0.5
     gl_FragData[3] = vec4(EncodeDepth(fdepth), 0.0);//
   #else
-    gl_FragColor =  vec4(EncodeDepth(fdepth), 0.0);
+    gl_FragColor =  vec4(totalDistance ,0. , 0. , 0.);
   #endif
 }
